4e62c01ffeeb1398d75616b339ea3f340f7c8920
Andrey Kopylkov <andreykopylkov@gmail.com>
2020-09-05 00:10:43 +0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed; //параметр скорости
+    public float jumpHeight; //высота прыжка
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Flip();
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y); //перемещаем персонажа по горизонатали
+        if (Input.GetKeyDown(KeyCode.Space)) //прыжок при нажатии пробела
+            rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse); //добовляем импульс
+    }
+
+    void Flip() //поворачиваем объект
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        if (Input.GetAxis("Horizontal") < 0)
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+    }
+}
