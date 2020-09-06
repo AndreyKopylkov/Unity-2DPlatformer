7bd1f3f8fc94f1fafb8267f0bb665e19974ce55e
Andrey Kopylkov <andreykopylkov@gmail.com>
2020-09-07 00:29:41 +0300

    Added death condition and scene restart

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     public float speed; //параметр скорости
     public float jumpHeight; //высота прыжка
     public Transform groundCheck;
+    private Transform transform;
     private bool isGrounded;
     private CapsuleCollider2D _box;
     private Animator anim;
@@ -15,6 +16,8 @@ public class Player : MonoBehaviour
     private int curHP; //текущее хп
     private int maxHP = 3; //максимальное хп игрока
     private bool isHit = false;
+    private float loseTime = 1f; //отсчет для перезапуска сцены
+    public Main main;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +25,7 @@ public class Player : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         _box = GetComponent<CapsuleCollider2D>();
         anim = GetComponent<Animator>();
+        transform = GetComponent<Transform>();
         curHP = maxHP;
     }
 
@@ -76,27 +80,42 @@ public class Player : MonoBehaviour
         curHP = curHP + deltaHP;
         print(curHP);
         if (deltaHP < 0)
+        {
             GetComponent<SpriteRenderer>().color = new Color(1f, 0f, 0f); //меняет цвет игрока при уроне
             StopCoroutine(OnHit());
             isHit = true;
             StartCoroutine(OnHit());
-        
+        }
+
         if (curHP <= 0)
+        {
             print("you are death");
+            GetComponent<CapsuleCollider2D>().enabled = false;
+            transform.localRotation = Quaternion.Euler(0, 0, 90);
+            Invoke("Lose", loseTime);// вызов проигрыша через loseTime сек
+        }
+
     }
 
     IEnumerator OnHit()
     {
-        if(isHit)
+        if (isHit)
             GetComponent<SpriteRenderer>().color = new Color(1f, GetComponent<SpriteRenderer>().color.g + 0.04f,
                 GetComponent<SpriteRenderer>().color.b + 0.04f); //меняет цвет игрока при уроне
 
         if (GetComponent<SpriteRenderer>().color.g == 1)
-            isHit = false;
+        { 
+            isHit = false; 
             StopCoroutine(OnHit());
-            
+        }
+
         yield return new WaitForSeconds(0.02f);
         StartCoroutine(OnHit());
+    }
+
+    void Lose() //вызов метода другого класса
+    {
+        main.GetComponent<Main>().Lose();
     }
 
     // private void CheckGround() //проверка стоит герой на земле или нет
