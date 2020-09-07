7f58698944d78a30d3f7847c09288bafa55af1a7
Andrey Kopylkov <andreykopylkov@gmail.com>
2020-09-07 13:02:32 +0300

    Added enemy Barnecle

---
+++ Assets/Scripts/Barnecle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barnecle : MonoBehaviour
+{
+    public float speed = 4f; 
+    private bool isWait = false;
+    private bool isHidden = true;
+    public float waitTime = 4f; //время ожилания
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
