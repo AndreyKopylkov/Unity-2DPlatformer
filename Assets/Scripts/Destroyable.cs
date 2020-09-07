5af5d71f0c10b7d709e46f1b58254391b6f0bcea
Andrey Kopylkov <andreykopylkov@gmail.com>
2020-09-07 22:22:53 +0300

    Adding the function of killing enemies and others fix

--- Assets/Scripts/Destroyable.cs
+++ Assets/Scripts/Destroyable.cs
@@ -3,32 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Destroyable : MonoBehaviour
 {
     private float repulsiveForce = 8f; //сила отталкивания от врага
-    public bool isAttacking = true; 
     
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player" && isAttacking)
+        if (other.gameObject.tag == "Player")
         {
-            print("Hit");
-            other.gameObject.GetComponent<Player>().RecountHP(-1);
             other.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * repulsiveForce, ForceMode2D.Impulse); //добовляем импульс
+            GetComponent<Animator>().SetInteger("State", 2); //Включаем анимацию смерти
+            GetComponent<SpriteRenderer>().color = new Color(1f, 0.5f, 0.5f, 0.8f);
+            GetComponent<Enemy>().isAttacking = false;
+            Destroy(gameObject, 1f);
         }
-    }
-
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
