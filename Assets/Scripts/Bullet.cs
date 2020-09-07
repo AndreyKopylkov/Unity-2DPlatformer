8e3fc3e47bbf5c6fa61388eb17aa4bbd0e8af1aa
Andrey Kopylkov <andreykopylkov@gmail.com>
2020-09-07 12:53:56 +0300

    Bullet script improvements

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,20 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private float timeToDisable = 10f;
     public float speed = 3f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SetDisabled());
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.down * speed * Time.deltaTime); //движение вниз
+    }
+
+    IEnumerator SetDisabled()
+    {
+        yield return new WaitForSeconds(timeToDisable);
+        gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        StopCoroutine(SetDisabled());
+        gameObject.SetActive(false);
     }
 }
